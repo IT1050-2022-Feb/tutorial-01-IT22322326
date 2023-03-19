b946cefbd6d89fa6e93b46fdd0dcb8b26d66c728
IT22322326 <128279270+IT22322326@users.noreply.github.com>
2023-03-19 12:27:11 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,21 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
+   //declare veriables
+   int m1;
+   int m2;
+   float avg;
+   //input marks
+   printf("Enter a mark 1:");
+   scanf("%d",&m1);
+   printf("Enter a mark 2:");
+   scanf("%d",&m2);
+   //calculations
+   avg=(m1+m2)/2.00;
+   //print the average marks
+   printf("Average is %.2f",avg);
   
   return 0;
 }
